41c05865a5e22bc1481d398875f65cc8e55d5975
sashimi36 <nikitrif@gmail.com>
2024-09-05 14:17:29 -0500

    fix formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { Analytics } from "./Analytics.tsx";
+import { Analytics } from "./Analytics";
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
