b781e294ac7e4da4b2e5a72aa33819afc0b74e87
sashimi36 <nikitrif@gmail.com>
2024-09-06 14:13:13 -0500

    update projects related copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import {useEffect, useState} from "react";
 
 
 function App() {
@@ -6,40 +7,39 @@ function App() {
 
   return (
     <>
-      <img className="profile-pic" src="/profilepic.jpg" alt="" />
+      <img className="profile-pic" src="/profilepic.jpg" alt="Shot in New York"/>
 
       <h1>
         Nikita Trifan, a developer
       </h1>
 
       <p>
-        I build instant interfaces designed to scale
+        Instant interfaces designed to scale
       </p>
 
       <h2>Work</h2>
-      <ul>
-        <li>Unfazed, spiritual awakening through the power of AI <i>(Coming soon)</i></li>
-        <li>Hashout, the future of remote work, decentralized <i>(Soon)</i></li>
-        <li>Junoswap, an interchain decentralized exchange focused on CW-20 asset adoption <i>(Soon)</i></li>
-        <li>Frame.io, a world-class video collaboration platform <i>(Soon)</i></li>
-        <li>US Mobile, the best carrier product in the nation <i>(Soon)</i></li>
-        <li>EXP digital, an international product studio <i>(Soon)</i></li>
-      </ul>
+      <p>I've done a variety of projects, from AI powered mobile apps and Blockchain interfaces to world-class
+        collaboration tools and DevTools. <i>(Reach out for a walkthrough)</i>
+      </p>
 
       <h2>Magazine</h2>
       <ul>
-        <li><a href="https://medium.com/@nikitatrifan/as-a-user-i-do-not-want-to-wait-c280e21373b6">As a user, I don't want to wait</a></li>
-        <li>I built the fastest yet smallest animation library for React <i>(Coming soon)</i></li>
+        <li><a href="https://medium.com/@nikitatrifan/as-a-user-i-do-not-want-to-wait-c280e21373b6">As a user, I don't
+          want to wait</a></li>
         <li>Perceived performance is all that matters <i>(Coming soon)</i></li>
+        <li>I got you the fastest animation library for React <i>(Coming soon)</i></li>
         <li>A tiny routing solution to abstract layout animations in React <i>(Coming soon)</i></li>
         <li>Who ate my App Store lunch? <i>(Coming soon)</i></li>
         <li>Brave & bold request making for interface performance <i>(Coming soon)</i></li>
-        <li><a href="https://medium.com/@nikitatrifan/is-my-state-in-react-going-to-cost-me-714fdd192ba7">Is my state in React going to cost me?</a></li>
+        <li><a href="https://medium.com/@nikitatrifan/is-my-state-in-react-going-to-cost-me-714fdd192ba7">Is my state in
+          React going to cost me?</a></li>
       </ul>
 
       <h2>Open source</h2>
       <ul>
-        <li><a href="https://github.com/trifanio/react-hinge">React Hinge</a> - The fastest way to animate components in React</li>
+        <li><a href="https://github.com/trifanio/react-hinge">React Hinge</a> - The fastest way to animate components in
+          React
+        </li>
         <li><i>(Coming soon)</i> A tiny router solution for layout animations in React</li>
       </ul>
 
